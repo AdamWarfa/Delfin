2af93741d0bea60be7e01746beaa4a92aec8f613
yousra-alt <123966037+yousra-alt@users.noreply.github.com>
2023-05-12 11:47:46 +0200

    opret medlemsskab

--- signup.js
+++ signup.js
@@ -52,6 +52,12 @@ async function signUpClciked(event) {
   const firstName = form.firstname.value;
   const lastName = form.lastname.value;
   const birthday = form.birthday.value;
+  const street = form.street.value;
+  const houseNumber = form.houseNumber.value;
+  const postCode = form.postCode.value;
+  const city = form.city.value;
+  const email = form.email.value;
+  const phoneNumber = form.phoneNumber.value;
   const memberType = form.memberType.value;
   const ageGroup = form.ageGroup.value;
   const levelType = form.levelType.value;
@@ -60,21 +66,28 @@ async function signUpClciked(event) {
     swimTypes.push(checkbox.value);
   });
 
-  const response = await createMember(firstName, lastName, birthday, memberType, ageGroup, levelType, swimTypes);
+  const response = await createMember(firstName, lastName, birthday, street, houseNumber, postCode, city, email, phoneNumber, memberType, ageGroup, levelType, swimTypes);
   if (response.ok) {
     console.log("New movie succesfully added to Firebase 🔥");
   }
 }
 
-async function createMember(firstName, lastName, birthday, memberType, ageGroup, levelType, swimTypes) {
+async function createMember(firstName, lastName, birthday, street, houseNumber, postCode, city, email, phoneNumber, memberType, ageGroup, levelType, swimTypes) {
   const newMember = {
     firstName: firstName,
     lastName: lastName,
     birthday: birthday,
+    street: street,
+    houseNumber: houseNumber,
+    postCode: postCode,
+    city: city,
+    email: email,
+    phoneNumber: phoneNumber,
     memberType: memberType,
     ageGroup: ageGroup,
     levelType: levelType,
     swimTypes: swimTypes,
+
   };
   const json = JSON.stringify(newMember);
 
