a9dddb968b4e11ba37d0c83f5cc2d58f8e00e1b7
Nouhayla-code <nouhayla@hotmail.dk>
2023-05-18 11:38:58 +0200

    log ind fixet

--- log-ind.js
+++ log-ind.js
@@ -4,6 +4,7 @@ window.addEventListener("load", initAuth);
 
 function initAuth() {
   runApp();
+  document.querySelector("#btn-sign-out").addEventListener("click", userIsSignedOut);
 
   const user = localStorage.getItem("authUser");
 
@@ -15,15 +16,19 @@ function initAuth() {
 }
 function userIsSignedIn() {
   location.hash = "#home-link";
-  document.querySelector("nav").classList.remove("hide");
+  // document.querySelector("nav").classList.remove("hide");
+  document.querySelector("#treasurer-link").classList.remove("log-in-hidden");
+  alert("Du er logget ind! ;)");
 }
 function userIsSignedOut() {
   location.hash = "#login-section";
-  document.querySelector("nav").classList.add("hide");
+  // document.querySelector("nav").classList.add("hide");
+  document.querySelector("#treasurer-link").classList.add("log-in-hidden");
+  alert("Du er logget ud!");
 }
 
 function runApp() {
-  document.querySelector("#btn-sign-up").addEventListener("submit", login);
+  document.querySelector("#login-form").addEventListener("submit", login);
   // document.querySelector("#btn-sign-out").addEventListener("click", signOutUser);
 }
 function login(event) {
@@ -31,7 +36,7 @@ function login(event) {
   const mail = event.target.mail.value;
   const password = event.target.password.value;
 
-  if (mail === "test@kea.dk" && password === "test123") {
+  if (mail === "kasper@kea.dk" && password === "test123") {
     localStorage.setItem("authUser", mail);
     document.querySelector("#signin-message").textContent = "";
 
@@ -45,7 +50,7 @@ function signOutUser() {
   localStorage.removeItem("authUser");
   userIsSignedOut();
 
-  // allerede udkommenteret 
+  // allerede udkommenteret
   // location.hash = "#home-section";
   //  document.querySelector("nav").classList.remove("hide");
   //  console.log("User signed out");
