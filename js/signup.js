eccb492b4a4a7dfeab89f49228d064aad8d2c0c3
Mads <mk@magnutron.dk>
2023-05-25 12:03:15 +0200

    Done

--- js/signup.js
+++ js/signup.js
@@ -22,7 +22,7 @@ async function signUpClicked(event) {
   const restance = false;
   const swimTypes = [];
 
-  checkboxes.forEach(checkbox => {
+  checkboxes.forEach((checkbox) => {
     swimTypes.push(checkbox.value);
   });
 
@@ -86,7 +86,6 @@ function getAge(birthday) {
   }
   return age;
 }
-
 
 let motionistRadioButton = document.querySelector("#motionist-member-type");
 let konkurrencesvømmerRadioButton = document.querySelector("#konkurrencesvømmer-member-type-update");
