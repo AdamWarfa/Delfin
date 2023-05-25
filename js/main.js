e7251eddcb5dc11bef031f9cb8014c864d0c8e20
sassnoe <105166994+sassnoe@users.noreply.github.com>
2023-05-25 11:46:23 +0200

    top5 modulering

--- js/main.js
+++ js/main.js
@@ -1,20 +1,17 @@
-"use strict";
-
-import { getResults, getMember } from "./rest-service.js";
-import { updateTrainerPage, createResultClicked, deleteResultClicked, inputResultSearchChanged, sortBy } from "./trainer.js";
+import { loginPage } from "./log-ind.js";
 import { signUpClicked } from "./signup.js";
 import { updateUsersGrid } from "./treasurer.js";
-import { loginPage } from "./log-ind.js";
+import { updateResultsGrid } from "./top5.js";
+import { updateTrainerPage, createResultClicked, deleteResultClicked, sortBy } from "./trainer.js";
 
 window.addEventListener("load", initApp);
 
 function initApp() {
   globalEventListeners();
   initViews();
-  updateResultsGrid();
   loginPage();
-
   updateUsersGrid();
+  updateResultsGrid();
   updateTrainerPage();
 }
 
@@ -22,8 +19,6 @@ function globalEventListeners() {}
 document.querySelector("#form-delete-result").addEventListener("submit", deleteResultClicked);
 document.querySelector("#trainer-link").addEventListener("click", updateTrainerPage);
 document.querySelector("#createResultForm").addEventListener("submit", createResultClicked);
-// document.querySelector("#input-search-result").addEventListener("keyup", inputResultSearchChanged);
-// document.querySelector("#input-search-result").addEventListener("search", inputResultSearchChanged);
 
 document.querySelector("#sortBySwimmer").addEventListener("click", () => sortBy("Swimmer"));
 document.querySelector("#sortByDiscipline").addEventListener("click", () => sortBy("Discipline"));
@@ -73,8 +68,8 @@ function setActiveLink(view) {
 
 function hideAllViews() {
   // remove .active for all .view-content elements (all views) and .view-link elements (all links)
-  document.querySelectorAll(".view-content").forEach(link => link.classList.remove("active"));
-  document.querySelectorAll(".view-link").forEach(link => link.classList.remove("active"));
+  document.querySelectorAll(".view-content").forEach((link) => link.classList.remove("active"));
+  document.querySelectorAll(".view-link").forEach((link) => link.classList.remove("active"));
   closeDropdowns();
 }
 
@@ -124,73 +119,5 @@ function closeDropdowns() {
     accountingMenu.classList.add("menu-closed");
   } else {
     return null;
-  }
-}
-
-async function updateResultsGrid() {
-  const listOfResults = await getResults();
-  showAllTop5(listOfResults);
-}
-
-function showAllTop5(listOfResults) {
-  const sortedResults = listOfResults.sort(sortTop5);
-  console.log(sortedResults);
-  document.querySelector("#front-grid-crawl").innerHTML = "";
-  document.querySelector("#front-grid-brystsvømning").innerHTML = "";
-  document.querySelector("#front-grid-butterfly").innerHTML = "";
-  document.querySelector("#front-grid-rygcrawl").innerHTML = "";
-
-  const filteredResultsCrawl = sortedResults.filter(result => result.discipline.includes("Crawl")).slice(0, 5);
-  console.log(filteredResultsCrawl);
-
-  const filteredResultsBrystsvømning = sortedResults.filter(result => result.discipline.includes("Brystsvømning")).slice(0, 5);
-  console.log(filteredResultsBrystsvømning);
-
-  const filteredResultsButterfly = sortedResults.filter(result => result.discipline.includes("Butterfly")).slice(0, 5);
-  console.log(filteredResultsButterfly);
-
-  const filteredResultsRygcrawl = sortedResults.filter(result => result.discipline.includes("Rygcrawl")).slice(0, 5);
-  console.log(filteredResultsRygcrawl);
-
-  showTop5(filteredResultsCrawl, "crawl");
-  showTop5(filteredResultsBrystsvømning, "brystsvømning");
-  showTop5(filteredResultsButterfly, "butterfly");
-  showTop5(filteredResultsRygcrawl, "rygcrawl");
-}
-
-function sortTop5(a, b) {
-  return a.time.localeCompare(b.time);
-}
-// Funktion til DOM-manipulation
-
-async function showTop5(results, discipline) {
-  for (const result of results) {
-    try {
-      console.log(result);
-
-      const user = await getMember(result.swimmer);
-      console.log(user);
-
-      console.log("#front-grid-" + discipline);
-      const grid = document.querySelector("#front-grid-" + discipline);
-      console.log(grid);
-
-      grid.insertAdjacentHTML(
-        "beforeend",
-        /*html*/ `
-
-<article class="top5-card">
-  <h2>${user.firstName} ${user.lastName}</h2>
-  <p>${user.ageGroup}</p>
-  <p>${result.meetName}</p>
-  <p>${result.discipline}</p>
-  <p>${result.time}</p>
-
-</article>
-`
-      );
-    } catch (error) {
-      console.log(`fejl i result ${result.id}`);
-    }
   }
 }
